5567ad873967e19ef5ecc102a0e4f5e8779a23c1
joe <joe.holmes87@gmail.com>
2021-02-05 08:28:23 -0500

    changed connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from "@nestjs/mongoose"
-import { BooksController } from './books/books.controller';
-import { BooksService } from './books/books.service';
 import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     BooksModule,
-    MongooseModule.forRoot("mongodb+srv://nestjstut:ZcxOkQHk4KgglMxp@cluster0.e9tf6.mongodb.net/test?retryWrites=true&w=majority"),
+    MongooseModule.forRoot("YOUR_CONNECTION_STRING_HERE"),
   ],
   controllers: [AppController],
   providers: [AppService ],
